a4cc0bb8b97bc231d0d5083cdf3b0a21a06ee4ef
Alex Dzyuba <alexeydzyuba27@gmail.com>
2026-01-16 17:56:24 +0300

    i

---
+++ legacy/main.js
@@ -0,0 +1,2 @@
+console.log(text.textContent)
+console.log(text.textContent.match(/\+\d to (?!All).*/g))
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, useTemplateRef } from 'vue'
+
+const text = useTemplateRef('text')
+
+setTimeout(() => {
+  console.log(text.value.textContent)
+  console.log(text.value.textContent.match(/\+\d to (?!All).*/g))
+})
+</script>
+
+<template>
+  <main>
+    <section>
+      <input id="input">
+    </section>
+
+    <section ref="text">
+      +1 to All Skills<br>
+      +2 to All Skills<br>
+
+      +3 to Rabies<br>
+      +3 to Cold Skills<br>
+      +4 to Fire Skills<br>
+      +4 to Corpse Exp<br>
+      +2 to Assasinate<br>
+    </section>
+  </main>
+</template>
